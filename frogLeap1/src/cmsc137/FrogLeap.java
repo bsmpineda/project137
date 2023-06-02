4e1286a5c0329fa99078b01387ce4d9c05e4de6c
ercapu <70846033+EarlCapuchino@users.noreply.github.com>
2023-06-03 03:00:40 +0800

    feat: word spelling powerups
    - leap 2* length of word entered
    - refer to the words.txt for directory of words

---
+++ frogLeap1/src/cmsc137/FrogLeap.java
@@ -0,0 +1,349 @@
+package cmsc137;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.FocusListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.image.BufferedImage;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+/**
+ * The game client itself!
+ * @author Joseph Anthony C. Hermocilla
+ *
+ */
+
+public class FrogLeap extends JPanel implements Runnable, Constants{
+	/**
+	 * Main window
+	 */
+	JFrame frame= new JFrame();
+	
+	/**
+	 * Player position, speed etc.
+	 */
+	int x=10,xspeed=1,yspeed=1,prevX,prevY;
+	int y = 30;
+	boolean isJump, isDown, winner, isF, isR = false;
+	/**
+	 * Game timer, handler receives data from server to update game state
+	 */
+	Thread t=new Thread(this);
+	
+	/**
+	 * Nice name!
+	 */
+	String name="Joseph";
+	
+	/**
+	 * Player name of others
+	 */
+	String pname;
+	
+	/**
+	 * Server to connect to
+	 */
+	String server="localhost";
+
+	/**
+	 * Flag to indicate whether this player has connected or not
+	 */
+	boolean connected=false, start = false;
+	
+	/**
+	 * get a datagram socket
+	 */
+    DatagramSocket socket = new DatagramSocket();
+
+	
+    /**
+     * Placeholder for data received from server
+     */
+	String serverData;
+	
+	/**
+	 * Offscreen image for double buffering, for some
+	 * real smooth animation :)
+	 */
+	BufferedImage offscreen, frog, bg, initBG;
+	JTextField chatInput = new JTextField("Chat here...");
+	JTextArea chatArea = new JTextArea("CHAT WINDOW:\n");
+	int gameWidth = 640;
+	int gameHeight = 480;
+	Color color = new Color(255,255,255);
+	String randomWord;
+	/**
+	 * Basic constructor
+	 * @param server
+	 * @param name
+	 * @throws Exception
+	 */
+	public FrogLeap(String server,String name) throws Exception{
+		this.server=server;
+		this.name=name;
+		
+		chatInput.setBounds(640,410,300,30);
+		chatInput.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e){
+				chatInput.setText("");
+			}
+
+			
+		});
+
+		chatInput.addKeyListener(new KeyAdapter(){
+			public void keyPressed(KeyEvent ke){
+				if(ke.getKeyCode()==KeyEvent.VK_ENTER){
+					if(!(chatInput.getText().equals(""))){
+						send("CHAT~`~"+name+"~`~"+chatInput.getText());
+						if (randomWord.equals(chatInput.getText().toUpperCase())) { // if the word matches the random word, leap forward
+							x+=randomWord.length()*2; // the movement is based on length of words
+							send("GET "+name+" "+chatInput.getText().toUpperCase()+" "+x+" "+y);
+						}
+					}
+					chatInput.setText("Chat here...");
+					frame.requestFocusInWindow();
+				}else if(ke.getKeyCode()==KeyEvent.VK_ESCAPE){
+					chatInput.setText("Chat here...");
+					frame.requestFocusInWindow();
+				}
+			}
+		});
+
+		
+		chatArea.setLineWrap(true);
+		chatArea.setFocusable(false);
+		JScrollPane scroll = new JScrollPane (chatArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scroll.setBounds(640,0,300,410);
+		
+		frame.setTitle(APP_NAME+":"+name);
+		//set some timeout for the socket
+		socket.setSoTimeout(100);
+		
+		//Some gui stuff i hate.
+		frame.getContentPane().add(chatInput);
+		frame.getContentPane().add(scroll);
+		frame.setFocusable(true);
+		frame.requestFocusInWindow();
+		frame.getContentPane().add(this);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(940, gameHeight);
+		frame.setVisible(true);
+		
+		//create the buffer
+		offscreen=new BufferedImage(gameWidth, gameHeight, BufferedImage.TYPE_INT_ARGB);;
+		frog = ImageIO.read(getClass().getResourceAsStream("images/frog.png"));
+		bg =ImageIO.read(getClass().getResourceAsStream("images/pond.png"));
+		initBG = ImageIO.read(getClass().getResourceAsStream("images/matching_screen.jpg"));
+
+		//Some gui stuff again...
+		frame.addMouseMotionListener(new MouseMotionHandler());
+		frame.addKeyListener(new KeyHandler());		
+		
+
+		//tiime to play
+		t.start();		
+	}
+	
+	/**
+	 * Helper method for sending data to server
+	 * @param msg
+	 */
+	public void send(String msg){
+		try{
+			byte[] buf = msg.getBytes();
+        	InetAddress address = InetAddress.getByName(server);
+        	DatagramPacket packet = new DatagramPacket(buf, buf.length, address, PORT);
+        	socket.send(packet);
+        }catch(Exception e){}
+		
+	}
+	
+	/**
+	 * The juicy part!
+	 */
+	public void run(){
+		while(true){
+			try{
+				Thread.sleep(10);
+			}catch(Exception ioe){}
+						
+			//Get the data from players
+			byte[] buf = new byte[256];
+			DatagramPacket packet = new DatagramPacket(buf, buf.length);
+			try{
+     			socket.receive(packet);
+			}catch(Exception ioe){/*lazy exception handling :)*/}
+			
+			serverData=new String(buf);
+			serverData=serverData.trim();
+			
+			//if (!serverData.equals("")){
+			//	System.out.println("Server Data:" +serverData);
+			//}
+
+			//Study the following kids. 
+			if (!connected && serverData.startsWith("CONNECTED")){
+				connected=true;
+				System.out.println("Connected.");
+				//start = true;
+				offscreen.getGraphics().drawImage(initBG, 0, 0, gameWidth, gameHeight, getBackground(), frame);
+				//show the changes
+				frame.repaint();
+			}else if (!connected){
+				System.out.println("Connecting..");				
+				send("CONNECT "+name);
+			}else if (connected){
+
+				//To show the sprites even if the space key was not clicked
+				if(serverData.startsWith("START")){
+					send("PLAYER "+name+" "+x+" "+y);
+					//start = false;
+				}
+
+				if (serverData.startsWith("PLAYER")){
+					offscreen.getGraphics().clearRect(0, 0, gameWidth, gameHeight);
+					offscreen.getGraphics().drawImage(bg, 0, 0, gameWidth, gameHeight, getBackground(), frame);
+
+					String[] playersInfo = serverData.split(":");
+					for (int i=0;i<playersInfo.length;i++){
+						//System.out.println(playersInfo.length);
+						String[] playerInfo = playersInfo[i].split(" ");
+						String pname =playerInfo[1];
+						int playerX = Integer.parseInt(playerInfo[2]);
+						int playerY = Integer.parseInt(playerInfo[3])*((2*i+6));
+						//draw on the offscreen image	
+						offscreen.getGraphics().drawImage(frog, playerX, playerY, 80, 80, frame); //draw the frog
+						offscreen.getGraphics().drawString(pname,playerX+30,
+						playerY+65);					
+					}
+					//show the changes
+					frame.repaint();
+				}else if(serverData.startsWith("CHAT")){
+					
+					String[] chatInfo = serverData.split("~`~");
+					if(chatInfo[1].equals(name)){
+						chatArea.append("[YOU] : "+chatInfo[2]+"\n");
+						
+					}else{
+						chatArea.append("["+chatInfo[1]+"] : "+chatInfo[2]+"\n");
+					}
+
+				}else if(serverData.startsWith("POWERUP")){
+					chatArea.setText("");
+					String[] chatInfo = serverData.split(" ");
+					randomWord = chatInfo[1];
+					chatArea.append("[WORD] : "+chatInfo[1]+"\n");
+				}else if(serverData.startsWith("RESET")){
+					// Format : RESET + <RESET_TOKEN> + <pname>
+					chatArea.setText("");
+					String[] chatInfo = serverData.split(" ");
+					randomWord = chatInfo[1];
+					chatArea.append(chatInfo[2]+" got the correct answer!"+"\n");
+				}else if(serverData.startsWith("WIN")){
+					 chatArea.setText("");
+					 Font font = new Font("Arial", Font.BOLD, 15);
+					 chatArea.setFont(font);
+					 chatArea.setForeground(Color.BLUE);
+					String[] chatInfo = serverData.split(" ");
+					chatArea.append(chatInfo[1]+" won the game!"+"\n");
+					
+				}else if (serverData.startsWith("END")) {
+					xspeed = 0;
+					yspeed = 0;
+					
+				}
+			}			
+		}
+	}
+	
+	/**
+	 * Repainting method
+	 */
+	public void paintComponent(Graphics g){
+		// Set the font color
+         // Set the font color
+		 g.setColor(Color.WHITE);
+        
+		 // Set the font
+		 Font font = new Font("Arial", Font.BOLD, 30);
+		 g.setFont(font);
+		 
+		g.drawImage(offscreen, 0, 0, null);
+		
+	}
+	
+	class MouseMotionHandler extends MouseMotionAdapter{
+		public void mouseMoved(MouseEvent me){
+			if(me.getID()==MouseEvent.MOUSE_CLICKED){
+				x=me.getX();y=me.getY();
+				if (x<=gameWidth){
+					frame.requestFocusInWindow();
+				}else{
+					chatInput.requestFocusInWindow();
+				}
+			}
+							
+		}
+	}
+	
+
+	class KeyHandler extends KeyAdapter{
+		public void keyPressed(KeyEvent ke){
+			switch (ke.getKeyCode()){
+			case KeyEvent.VK_LEFT:
+				if (!isJump) {
+					x+=xspeed;
+					y-=yspeed;
+					send("PLAYER "+name+" "+x+" "+y);
+					isJump = true;
+					isDown = false;
+				}
+				break;
+			case KeyEvent.VK_RIGHT:
+				if (!isDown) {
+					y+=yspeed;
+					send("PLAYER "+name+" "+x+" "+y);
+					isDown = true;
+					isJump = false;
+				}
+				break;
+			case KeyEvent.VK_ENTER:
+				chatInput.requestFocusInWindow();
+				break;
+			}
+			if (x==gameWidth-80) {
+				xspeed = 0;
+				send("WIN "+name);
+			}
+			
+		
+		}
+	}
+	
+	
+	public static void main(String args[]) throws Exception{
+		if (args.length != 2){
+			System.out.println("Usage: java -jar FrogLeapeap-client <server> <player name>");
+			System.exit(1);
+		}
+
+		new FrogLeap(args[0],args[1]);
+	}
+}
---
+++ frogLeap1/src/cmsc137/GameServer.java
@@ -0,0 +1,281 @@
+package cmsc137;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.lang.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The main game server. It just accepts the messages sent by one player to
+ * another player
+ * @author Joseph Anthony C. Hermocilla
+ *
+ */
+
+public class GameServer implements Runnable, Constants{
+	/**
+	 * Placeholder for the data received from the player
+	 */	 
+	String playerData;
+	
+	/**
+	 * The number of currently connected player
+	 */
+	public static int playerCount=0;
+	
+	/**
+	 * The socket
+	 */
+    DatagramSocket serverSocket = null;
+    
+    /**
+     * The current game state
+     */
+	GameState game;
+
+	/**
+	 * The current game stage
+	 */
+	int gameStage=WAITING_FOR_PLAYERS;
+	
+	/**
+	 * Number of players
+	 */
+	int numPlayers;
+	
+	/**
+	 * The main game thread
+	 */
+	Thread t = new Thread(this);
+	static String powerupData;
+    private static final String[] letters = {"LEAP", "FROG"};
+    private static final Random random = new Random();
+    static boolean is_F_server,  is_R_server, powerOn = false;
+    String randomWord;
+	Timer timer = new Timer();
+
+	
+	/**
+	 * Simple constructor
+	 */
+	public GameServer(int numPlayers){
+		this.numPlayers = numPlayers;
+		try {
+            serverSocket = new DatagramSocket(PORT);
+			serverSocket.setSoTimeout(100);
+		} catch (IOException e) {
+            System.err.println("Could not listen on port: "+PORT);
+            System.exit(-1);
+		}catch(Exception e){}
+		//Create the game state
+		game = new GameState();
+		
+		System.out.println("Game created...");
+		
+		//Start the game thread
+		t.start();
+	}
+	
+	/**
+	 * Helper method for broadcasting data to all players
+	 * @param msg
+	 */
+	public void broadcast(String msg){
+		for(Iterator ite=game.getPlayers().keySet().iterator();ite.hasNext();){
+			String name=(String)ite.next();
+			NetPlayer player=(NetPlayer)game.getPlayers().get(name);			
+			send(player,msg);	
+		}
+	}
+
+
+	/**
+	 * Send a message to a player
+	 * @param player
+	 * @param msg
+	 */
+	public void send(NetPlayer player, String msg){
+		DatagramPacket packet;	
+		byte buf[] = msg.getBytes();		
+		packet = new DatagramPacket(buf, buf.length, player.getAddress(),player.getPort());
+		try{
+			serverSocket.send(packet);
+		}catch(IOException ioe){
+			ioe.printStackTrace();
+		}
+	}
+	
+	public static String[] readWordsFromFile(String filename) {
+	        List<String> wordsList = new ArrayList<>();
+	        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+	            String line;
+	            while ((line = reader.readLine()) != null) {
+	                wordsList.add(line);
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	        return wordsList.toArray(new String[0]);
+	 }
+	
+	public static String getRandomWord(String[] words) {
+	     Random random = new Random();
+	     int index = random.nextInt(words.length);
+	     return words[index];
+	}
+	static void setPowerup(String powerupdata) {
+		powerupData = powerupdata;
+	}
+	
+	/**
+	 * The juicy part
+	 */
+	public void run(){
+		
+	  class LetterTask extends TimerTask {
+	        @Override
+	        public void run() {
+	        	String filename = "words.txt";
+	            String[] arr_words = readWordsFromFile(filename);
+	            
+	            if (arr_words != null && arr_words.length > 0) {
+	                randomWord = getRandomWord(arr_words).toUpperCase();
+	                powerupData = "POWERUP "+randomWord;
+	                broadcast(powerupData);
+	                powerOn = true; //this means that the powerUp is (NOT TAKEN)
+	            } else {
+	                System.out.println("No words found in the file.");
+	            }
+	        }
+	    }
+		    
+		timer.schedule(new LetterTask(), 0, 15000); // starts timer and call every 3 seconds
+		while(true){
+						
+			// Get the data from players
+			byte[] buf = new byte[256];
+			DatagramPacket packet = new DatagramPacket(buf, buf.length);
+			try{
+     			serverSocket.receive(packet);
+			}catch(Exception ioe){}
+			
+			/**
+			 * Convert the array of bytes to string
+			 */
+			playerData=new String(buf);
+			
+			//remove excess bytes
+			playerData = playerData.trim();
+			//if (!playerData.equals("")){
+			//	System.out.println("Player Data:"+playerData);
+			//}
+		
+			// process
+			switch(gameStage){
+				  case WAITING_FOR_PLAYERS:
+						//System.out.println("Game State: Waiting for players...");
+						if (playerData.startsWith("CONNECT")){
+							String tokens[] = playerData.split(" ");
+							NetPlayer player=new NetPlayer(tokens[1],packet.getAddress(),packet.getPort());
+							System.out.println("Player connected: "+tokens[1]);
+							game.update(tokens[1].trim(),player);
+							broadcast("CONNECTED "+tokens[1]);
+							playerCount++;
+							if (playerCount==numPlayers){
+								gameStage=GAME_START;
+							}
+						}
+					  break;	
+				  case GAME_START:
+					  System.out.println("Game State: START");
+					  broadcast("START");
+					  gameStage=IN_PROGRESS;
+					  break;
+					  
+				  case IN_PROGRESS:
+					  //Player data was received!
+					  if (playerData.startsWith("PLAYER")){
+						  //Tokenize:
+						  //The format: PLAYER <player name> <x> <y>
+						  String[] playerInfo = playerData.split(" ");					  
+						  String pname =playerInfo[1];
+						  int x = Integer.parseInt(playerInfo[2].trim());
+						  int y = Integer.parseInt(playerInfo[3].trim());
+						
+						  //Get the player from the game state
+						  NetPlayer player=(NetPlayer)game.getPlayers().get(pname);					  
+						  player.setX(x);
+						  player.setY(y);
+						  
+						  //System.out.println(pname + ": X=" + x + " Y="+ y);
+						  //Update the game state
+						  
+						  game.update(pname, player);
+						  //Send to all the updated game state
+						  broadcast(game.toString());
+					  }else if (playerData.startsWith("CHAT")){
+						System.out.println("Received chat...");
+						String[] playerInfo = playerData.split("~");
+						String chat = playerInfo[4].toUpperCase();
+						broadcast(playerData);
+					  }else if (playerData.startsWith("GET")){
+							String[] chat = playerData.split(" ");
+							String pname = chat[1];
+							System.out.println("Correct! "+pname);
+							String powerup = chat[2];
+							int x = Integer.parseInt(chat[3].trim());
+							int y = Integer.parseInt(chat[4].trim());
+							NetPlayer player=(NetPlayer)game.getPlayers().get(pname);					  
+							if (powerup.equals(randomWord)&& powerOn) {
+								powerOn = false; //this means that the powerUp is taken
+								System.out.println("Correct!");
+								player.setX(x);
+								player.setY(y);
+								game.update(pname, player);
+								//Send to all the updated game state
+								broadcast(game.toString());
+								powerupData = "RESET "+RESET_TOKEN+" "+pname; //reset the value of the powerup into long string
+					            broadcast(powerupData);
+							}else {
+								System.out.println("Wrong!");
+							}
+							broadcast(playerData);
+					  }else if (playerData.startsWith("WIN")){
+							String[] playerInfo = playerData.split(" ");
+							broadcast(playerData);
+							gameStage = END_GAME; //the game will end here
+					  }
+					  break;
+					 
+				}				  
+		}
+	}
+	
+	
+	 
+  
+	
+	
+	public static void main(String args[]){
+		if (args.length != 1){
+			System.out.println("Usage: java -jar circlewars-server <number of players>");
+			System.exit(1);
+		}
+		
+		new GameServer(Integer.parseInt(args[0]));
+	   
+	
+	}
+}
+
